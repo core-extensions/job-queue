fc95c057d30859017de78559ac20a0746afc41b5
Ramil Gallyamov <gallyamow@gmail.com>
2024-11-22 18:03:05 +0300

    DEPLOY: cs fix

--- src/Exception/TerminateJobException.php
+++ src/Exception/TerminateJobException.php
@@ -4,9 +4,35 @@ declare(strict_types=1);
 
 namespace CoreExtensions\JobQueueBundle\Exception;
 
+use Webmozart\Assert\InvalidArgumentException;
+
 /**
  * used to forced stop handling
  */
 final class TerminateJobException extends \RuntimeException implements JobNonRetryableExceptionInterface
 {
+    private string $jobId;
+
+    /**
+     * (only webmozart/assert)
+     */
+    public static function becauseOfAssertFail(string $jobId, InvalidArgumentException $assertionError): self
+    {
+        $res = new self(
+            sprintf(
+                'Job "%s" terminated by assertion fail.',
+                $jobId,
+            ),
+            0,
+            $assertionError
+        );
+        $res->jobId = $jobId;
+
+        return $res;
+    }
+
+    public function getJobId(): string
+    {
+        return $this->jobId;
+    }
 }
